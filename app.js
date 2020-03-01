60a8e97bf904e1167f696015aaf2002e88e26234
MIKIO ODA <oda.mikio@gmail.com>
2020-03-01 10:22:31 +0900

    ...

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var refactor1Router = require('./routes/refactor1');
+var typescriptRouter = require('./routes/typescript');
 
 var app = express();
 
@@ -20,7 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/refactor1', refactor1Router);
+app.use('/typescript', typescriptRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,149 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+router.get('/', function (req, res, next) {
+
+	res.send("OK!");
+
+});
+
+router.get('/insert', function (req, res, next) {
+	MongoClient.connect('mongodb://127.0.0.1:27017/myDB', (err, db) => {
+		if (!err) {
+			db.collection("myCollection", (err2, collection) => {
+				if (!err2) {
+					res.send("OK!");
+					db.close();
+				} else {
+					res.send(err.message);
+				}
+			})
+		} else {
+			res.send(err.message);
+		}
+	});
+});
+
+router.get('/insert2', function (req, res, next) {
+	MongoClient.connect('mongodb://localhost', (err, client) => {
+		if (!err) {
+			const db = client.db("myDB");
+			db.collection("myCollection", (err2, collection) => {
+				if (!err2) {
+					res.send("OK!");
+					client.close();
+				} else {
+					res.send(err2.message);
+				}
+			});
+		} else {
+			res.send(err.message);
+		}
+	});
+});
+
+router.get('/insert3', function (req, res, next) {
+	MongoClient.connect('mongodb://localhost', (err, client) => {
+		if (!err) {
+			const db = client.db("myDB");
+			db.collection("myCollection", (err2, collection) => {
+				if (!err2) {
+					const document = {name: "myRecord", content: "content"};
+					collection.insertOne(document, (err3, result) => {
+						if (!err3) {
+							res.send("OK!");
+							client.close();
+						} else {
+							res.send(err3.message);
+						}
+					});
+				} else {
+					res.send(err2.message);
+				}
+			});
+		} else {
+			res.send(err.message);
+		}
+	});
+});
+
+router.get('/query', function (req, res, next) {
+	MongoClient.connect('mongodb://localhost', (err, client) => {
+		if (!err) {
+			const db = client.db("myDB");
+			db.collection("myCollection", (err2, collection) => {
+				if (!err2) {
+					const query = {name: "myRecord"};
+					collection.find(query).toArray((err3, result) => {
+						if (!err3) {
+							res.send(JSON.stringify(result));
+							client.close();
+						} else {
+							res.send(err3.message);
+						}
+					});
+				} else {
+					res.send(err2.message);
+				}
+			});
+		} else {
+			res.send(err.message);
+		}
+	});
+});
+
+router.get('/update', function (req, res, next) {
+	MongoClient.connect('mongodb://localhost', (err, client) => {
+		if (!err) {
+			const db = client.db("myDB");
+			db.collection("myCollection", (err2, collection) => {
+				if (!err2) {
+					const where = {name: "myRecord"};
+					var set = {$set: {content: "update content!"}};
+					collection.updateOne(where, set, (err3, result) => {
+						if (!err3) {
+							res.send("OK!");
+							client.close();
+						} else {
+							res.send(err3.message);
+						}
+					});
+				} else {
+					res.send(err2.message);
+				}
+			});
+		} else {
+			res.send(err.message);
+		}
+	});
+});
+
+router.get('/delete', function (req, res, next) {
+	MongoClient.connect('mongodb://localhost', (err, client) => {
+		if (!err) {
+			const db = client.db("myDB");
+			db.collection("myCollection", (err2, collection) => {
+				if (!err2) {
+					const where = {name: "myRecord"};
+					collection.deleteOne(where, (err3, result) => {
+						if (!err3) {
+							res.send("OK!");
+							client.close();
+						} else {
+							res.send(err3.message);
+						}
+					});
+				} else {
+					res.send(err2.message);
+				}
+			});
+		} else {
+			res.send(err.message);
+		}
+	});
 });
 
 module.exports = router;
